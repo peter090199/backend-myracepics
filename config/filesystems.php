bf1a313b428844e98f42291d999b30237428ee4c
Peds Yorpo <pedroyorpo17@gmail.com>
2026-02-04 20:34:45 +0800

    dd

--- config/filesystems.php
+++ config/filesystems.php
@@ -11,7 +11,7 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DISK', 'public'),
+    'default' => env('FILESYSTEM_DISK', 'local'),
 
     /*
     |--------------------------------------------------------------------------
