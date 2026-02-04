1ce16e71c412e9e12dc4d08c6191d0b936b61225
Peds Yorpo <pedroyorpo17@gmail.com>
2026-02-04 22:54:02 +0800

    cc

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,16 +40,19 @@ return [
         ],
 
         // Standard S3 (AWS)
-      's3' => [
-        'driver' => 's3',
-        'key' => env('AWS_ACCESS_KEY_ID'),
-        'secret' => env('AWS_SECRET_ACCESS_KEY'),
-        'region' => env('AWS_DEFAULT_REGION'),
-        'bucket' => env('AWS_BUCKET'),
-        'url' => env('AWS_URL'),
-        'endpoint' => env('AWS_ENDPOINT'), // optional, if you use custom endpoint
-        'use_path_style_endpoint' => false,
-    ],
+     's3' => [
+            'driver' => 's3',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_BUCKET'),
+            'url' => env('AWS_URL'),
+            'endpoint' => env('AWS_ENDPOINT'),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', true),
+            'visibility' => 'public',
+            'throw' => true, // important to see exact errors
+        ],
+
 
 
         // // Backblaze B2 (S3-compatible)
