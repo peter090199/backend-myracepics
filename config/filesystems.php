ca0c76bc36163423d9c03b1a9f83fc6698a472c6
Peds Yorpo <pedroyorpo17@gmail.com>
2026-02-04 20:18:15 +0800

    commit

--- config/filesystems.php
+++ config/filesystems.php
@@ -11,7 +11,7 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DISK', 's3'),
+    'default' => env('FILESYSTEM_DISK', 'local'),
 
     /*
     |--------------------------------------------------------------------------
